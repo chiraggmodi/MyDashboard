73fc81cdbbedc3147833dea2558832aee025d46e
chiraggmodi <modichirag14@gmail.com>
2016-02-23 16:36:42 +0530

    Initial upload
    This project is still under process. So requesting not to download yet.

---
+++ keystone.js
@@ -0,0 +1,72 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone');
+
+keystone.init({
+
+	'name': 'dashboard',
+	'brand': 'dashboard',
+	
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'jade',
+	
+	'emails': 'templates/emails',
+	
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'Y',
+	'cookie secret': 'b7!*ZAg0:-R+v{>`$RHh,<pG.Msktfai|d]]9:Jn12mZ;i>sUniUi|e.ScM_p2GI'
+
+});
+
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
+});
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('email locals', {
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7'
+		}
+	}
+});
+
+keystone.set('email rules', [{
+	find: '/images/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+}, {
+	find: '/keystone/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+}]);
+
+keystone.set('email tests', require('./routes/emails'));
+
+keystone.set('nav', {
+	'posts': ['posts', 'post-categories'],
+	'galleries': 'galleries',
+	'enquiries': 'enquiries',
+	'ys': 'ys'
+});
+
+keystone.start();
